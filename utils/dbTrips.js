4451315eba025b03db532dea02c37184ca64feaf
dAVE Inden <davidinden@gmail.com>
2019-11-18 16:01:43 -0800

    update README to note order of returned trips and fix typo in db select

--- utils/dbTrips.js
+++ utils/dbTrips.js
@@ -1,11 +1,31 @@
 const db = require('../data/dbConfig');
 
 function getUserTrips(userId) {
-    return db('trips').select('id', 'title', 'description', 'isPrivate', 'isProfessional', 'image', 'duration', 'distance', 'date', 'triptype')
+    return db('trips').select('id', 'title', 'description', 'isPrivate', 'isProfessional', 'image', 'duration', 'distance', 'date', 'tripType')
         .where('user_id', userId)
         .orderBy('date');
 };
 
+function getById(tripId) {
+
+};
+
+function update(tripId) {
+
+};
+
+function deleteTrip(tripId) {
+
+};
+
+function add(trip) {
+
+};
+
 module.exports = {
-    getUserTrips
+    getUserTrips,
+    getById,
+    update,
+    deleteTrip,
+    add
 };
